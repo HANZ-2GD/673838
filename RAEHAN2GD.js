d59716bbc4c5d73712d656765afb0624afe5bb51
RAEHAN-SP <hanz.26r@gmail.com>
2026-04-10 19:00:47 +0800

    Update RAEHAN2GD.js

--- RAEHAN2GD.js
+++ RAEHAN2GD.js
@@ -342,7 +342,58 @@ break
 			
 			
 	break   		
-		
+		case 'ping': {
+				const used = process.memoryUsage()
+				const cpus = os.cpus().map(cpu => {
+					cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+					return cpu
+				})
+				const cpu = cpus.reduce((last, cpu, _, { length }) => {
+					last.total += cpu.total
+					last.speed += cpu.speed / length
+					last.times.user += cpu.times.user
+					last.times.nice += cpu.times.nice
+					last.times.sys += cpu.times.sys
+					last.times.idle += cpu.times.idle
+					last.times.irq += cpu.times.irq
+					return last
+				}, {
+					speed: 0,
+					total: 0,
+					times: {
+						user: 0,
+						nice: 0,
+						sys: 0,
+						idle: 0,
+						irq: 0
+					}
+				})
+				let timestamp = speed()
+				let latensi = speed() - timestamp
+				neww = performance.now()
+				oldd = performance.now()
+				respon = `Kecepatan Respon ${latensi.toFixed(4)} _Second_ \n ${oldd - neww} _miliseconds_\n\nRuntime : ${runtime(process.uptime())}\n\n💻 Info Server\nRAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}\n\n_NodeJS Memory Usaage_\n${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n')}\n\n${cpus[0] ? `_Total CPU Usage_\n${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}\n_CPU Core(s) Usage (${cpus.length} Core CPU)_\n${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}`.trim()
+				m.reply(respon)
+			}
+			break
+			 case 'speed': {
+				m.reply('Testing Speed...')
+				let cp = require('child_process')
+				let { promisify } = require('util')
+				let exec = promisify(cp.exec).bind(cp)
+				let o
+				try {
+					o = await exec('python3 speed.py --share')
+				} catch (e) {
+					o = e
+				} finally {
+					let { stdout, stderr } = o
+					if (stdout.trim()) m.reply(stdout)
+					if (stderr.trim()) m.reply(stderr)
+				}
+			}
+			break
+			
 			
 			
 			

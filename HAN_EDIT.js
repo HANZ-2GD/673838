69e1a89617909c9350d458245bb9940e568fa01d
RAEHAN-SP <hanz.26r@gmail.com>
2026-04-13 12:57:39 +0800

    Add files via upload

---
+++ HAN_EDIT.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+
+global.owner = ['6281241415567'] 
+global.listprefix = ['+','!','.']
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+
+
+
+
+
+
+// Lainnya
+
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+
+});
